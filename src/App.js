bcbe1c65c100c2d8c20962332d773c03d5e00c8e
SOFTCONSTRUCT\artur.arushanyan <arthur.arushanyan92@gmail.com>
2017-08-30 13:29:20 +0400

    setInterval added

--- src/App.js
+++ src/App.js
@@ -7,13 +7,30 @@ class App extends Component {
     constructor(props){
         super(props);
         this.state = {
-
+            td011:{
+                id: 1001,
+                priority: 2,
+                title: 'asdf001',
+                desicription: "desc001",
+                assignee: "Artur",
+                completed: false
+            }
         }
+    }
+    componentDidMount(){
+        setInterval(()=>{
+            this.setState({
+                td011:{
+                    id: Math.random()
+                }
+            })
+        },3000)
     }
   render() {
     return (
       <div className="App">
-        <Backlog/>
+          <h2>{this.state.td011.id}</h2>
+        <Backlog taskID = {this.state.td011.id}/>
         <Statistics/>
       </div>
     );
--- src/Components/Backlog.js
+++ src/Components/Backlog.js
@@ -2,10 +2,14 @@ import React,{Component} from 'react';
 import Todo from "./Todo";
 
 export default class Backlog extends Component{
+    constructor(props){
+        super(props);
+    }
     render(){
         return (
             <div className="backlog">
-                <p>I'm Backlog</p>
+                <p>I'm backlog</p>
+                <p>{this.props.taskID}</p>
                 <Todo/>
             </div>
         )
